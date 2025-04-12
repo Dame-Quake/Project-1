818bd4a11a8bfc94a2f2d41824fdeb1f30530c72
Damian Villarreal <villarrealdamian2002@gmail.com>
2025-04-12 13:37:39 -0600

    csv file upload

---
+++ src/CSVFileReader.java
@@ -0,0 +1,17 @@
+package src;
+/**
+ * The {@code CSVFileReader} class is responsible for reading and parsing CSV files 
+ * that contain data related to space objects such as debris, payloads, satellites, 
+ * and unknown objects. 
+ * <p>
+ * This class loads data from a CSV file into the system and prepares it for tracking 
+ * and analysis within the {@link MissionControl} system.
+ * </p>
+ *
+ * @author John Chavarria, Damian Villarreal
+ * @version 1.0
+ * @since 2025-04-12
+ */
+public class CSVFileReader {
+    
+}
