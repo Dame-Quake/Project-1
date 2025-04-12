7b23867900adc903efac9b9a5aba6f4b1b9664f9
Damian Villarreal <villarrealdamian2002@gmail.com>
2025-04-12 12:49:36 -0600

    Javadoc Comments

--- src/RunSimulation.java
+++ src/RunSimulation.java
@@ -1,18 +1,32 @@
 package src;
+/**
+ * The {@code RunSimulation} class serves as the entry point for the application.
+ * It initializes the necessary components and starts the simulation process.
+ *
+ * @author John Chavarria Damian Villarreal
+ * @since 2025-04-9
+ * @version 1.0
+ */
 public class RunSimulation {
     private MissionControl control;
     private boolean running;
 
+    /** Main method to initialize the simulation environment and starts the simulation */
     public static void main(String[] args){
         RunSimulation sim = new RunSimulation();
         sim.displayMainMenu();
     }
-
+    
+    /**
+     * Constructs a new {@code RunSimulation} instance and initializes the mission control system.
+     */
     public RunSimulation(){
         this.control = new MissionControl(new TrackingSystem());
     }
 
+    /** Displays the main menu for user interaction */
     public void displayMainMenu(){}
 
+    /** Handles user choices and interactions */
     public void handleUserChoice(String choice){}
 }
--- src/SpaceObject.java
+++ src/SpaceObject.java
@@ -1,4 +1,12 @@
 package src;
+/**
+ * Represents a general space object with common attributes such as identification,
+ * origin, orbital characteristics, and tracking information.
+ * <p>
+ * This abstract class serves as a base for specific types of space objects,
+ * providing shared properties and enforcing the implementation of the {@code getInfo()} method.
+ * </p>
+ */
 public abstract class SpaceObject {
     protected String recordID;
     protected String name;
