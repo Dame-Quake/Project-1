824795b66e057885535e0e97a251cfa14a4f6d57
Damian Villarreal <villarrealdamian2002@gmail.com>
2025-04-11 23:59:55 -0600

    Javadoc comments

--- src/Debris.java
+++ src/Debris.java
@@ -5,6 +5,16 @@ package src;
 public class Debris extends SpaceObject {
     public Debris(){
         this.name = name; 
+        this.recordID = recordID;
+        this.country = country;
+        this.orbitType = orbitType;
+        this.launchYear = launchYear;
+        this.launchSite = launchSite;
+        this.longitude = longitude;
+        this.avgLongitude = avgLongitude;
+        this.geohash = geohash;
+        this.daysOld = daysOld;
+        this.conjunctionCount = conjunctionCount;
     }
     @Override
     public String getInfo(){
--- src/MissionControl.java
+++ src/MissionControl.java
@@ -1,31 +1,56 @@
 package src;
 import java.util.*;
+/**
+ * @author John CHanvarria Damian Villarreal 
+ * @version 1.0
+ * @since 2025-04-9
+ * The {@code MissionControl} class manages the coordination of space object tracking,
+ * user sessions, and system logging. It acts as the central controller for the application,
+ * linking together users, tracking systems, and data operations.
+ */
 public class MissionControl {
+    
+    /** List of space objects being tracked.*/
     private List<SpaceObject> objects;
+    /** The current user of the system.*/
     private User currentUser;
+    /** The path to the log file.*/
     private String logFilePath;
+    /** The tracking system used for tracking space objects.*/
     private TrackingSystem trackingSystem;
+    /**
+     * Constructs a new {@code MissionControl} instance with the provided tracking system.
+     *
+     * @param trackingSystem the tracking system to be used by this controller
+     */
 
     public MissionControl(TrackingSystem trackingSystem){
         this.trackingSystem = trackingSystem;
     }
 
+    /**Sets the user currently interacting with the system */
     public void setCurrentUser(User user){
         this.currentUser = user;
     }
 
+    /**Initiates object tracking based on their types using the tracking system */
     public void performTracking(){
         trackingSystem.trackByType();
     }
-
+    /**Loads external or persisted data into the mission control system*/
     public void loadData(){}
 
+    /**Tracks all registered space objects in the system */
     public void trackObjects(){}
 
+    /**
+     * Retrieves the type of the currently active user
+     * @return the type of user as a {@code String}
+     */
     public String getUserType(){
         return null;
     }
-
+    /**Logs the user's activity to the appropriate log file */
     public void logActivity(){}
 
 
